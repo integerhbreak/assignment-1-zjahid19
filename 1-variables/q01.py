e2ae63f14527aaba2d660cc4d4289206a1c0c78a
Jahid Sheikh <zjahid19@gmail.com>
2025-07-03 14:14:44 +0000

    solved

--- 1-variables/q01.py
+++ 1-variables/q01.py
@@ -2,4 +2,5 @@
 # Create a variable to store your full name and print it.
 
 # Your code goes here
-
+fullname = 'zahid'
+print(fullname)
--- 1-variables/q02.py
+++ 1-variables/q02.py
@@ -2,4 +2,5 @@
 # Create a variable to store your age and print it.
 
 # Your code goes here
-
+age = 38
+print(age)
